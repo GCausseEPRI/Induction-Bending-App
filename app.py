3c3cc312749665c41b47ec56c7cbe302473744e0
GCausse <GCausse@epri.com>
2023-08-14 14:26:32 -0400

    Add EPRI Branding

---
+++ app.py
@@ -0,0 +1,12 @@
+
+# title for the web app; will appear in the browser tab and optionally in the sidebar
+site_title = r'Bending Analysis'
+
+# optional suffix to append to the title shown in the browser tab
+title_suffix = r'EPRI'
+
+# when True, the logo will appear in the upper-right corner of each page
+show_logo = True
+
+# when True, the site title will appear near the top of the sidebar of each page
+show_site_title_in_sidebar = True
